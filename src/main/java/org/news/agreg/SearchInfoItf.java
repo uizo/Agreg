43216e1a9d780755fdc33326927c5765272144a1
agauchy <anthony.gauchy@gmail.com>
2015-04-21 17:51:36 +0200

    Ajout des constantes pour les propriétés d'enregistrement et de recherche

--- src/main/java/org/news/agreg/SearchInfoItf.java
+++ src/main/java/org/news/agreg/SearchInfoItf.java
@@ -4,5 +4,10 @@ import java.net.URL;
 import java.util.Map;
 
 public interface SearchInfoItf {
-	Map<URL, String> search(String query);
+
+    public final static String PROP_TYPE = "type";
+    public final static String TYPE_DATAAGREG = "agregator";
+    public final static String TYPE_DATAFINDER = "finder";
+
+    Map<URL, String> search(String query);
 }
